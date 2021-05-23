4c66d2d1cd83504a5ba8df3a2d2854907c1b49ea
Evgeniy <antonm33@yandex.ru>
2021-05-23 20:06:16 +0200

    fix: compilation errors

--- models/game.js
+++ models/game.js
@@ -1,38 +1,43 @@
-function(sequelize, DataTypes) {
-    return sequelize.define('game', {
-        title: {
-            type: DataTypes.STRING(25),
-            allowNull: false,
-        },
+import Sequelize from 'sequelize';
+import db from '../db.js';
 
-        owner_id: {
-            type: DataTypes.INTEGER,
-            allowNull: false
-        },
+const createGameModel = function (sequelize, DataTypes) {
+  return sequelize.define('game', {
+    title: {
+      type: DataTypes.STRING(25),
+      allowNull: false,
+    },
 
-        studio: {
-            type: DataTypes.STRING,
-            allowNull: false,            
-        },
+    owner_id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
 
-        esrb_rating: {
-            type: DataTypes.CHAR(5),
-            allowNull: false,
-        },
+    studio: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
 
-        user_rating: {
-            type: DataTypes.INTEGER,
-            allowNull: false,
-            validate: {
-                min: 1,
-                max: 5
-            }
-        },
+    esrb_rating: {
+      type: DataTypes.CHAR(5),
+      allowNull: false,
+    },
 
-        have_played : {
-            type: DataTypes.BOOLEAN,
-            defaultValue: false,
-            allowNull: false
-        }
-    })
-}
+    user_rating: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      validate: {
+        min: 1,
+        max: 5,
+      },
+    },
+
+    have_played: {
+      type: DataTypes.BOOLEAN,
+      defaultValue: false,
+      allowNull: false,
+    },
+  });
+};
+
+export default createGameModel(db, Sequelize.DataTypes);
